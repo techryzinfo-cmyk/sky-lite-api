d9471e54907515ea1715943d59e1ae7627a4e6c1
maheshdelxn <maheshindalkar.delxn@gmail.com>
2026-06-17 16:02:54 +0530

    changes in risk manamgent

--- src/app/api/projects/[id]/issues/route.js
+++ src/app/api/projects/[id]/issues/route.js
@@ -15,8 +15,8 @@ export const GET = withAuth(async function (req, { params }) {
     await dbConnect();
 
     const issues = await Issue.find({ project: id })
-      .populate('createdBy', 'name email')
-      .populate('assignedTo', 'name email')
+      .populate('createdBy', 'name email __enc_name')
+      .populate('assignedTo', 'name email __enc_name')
       .sort({ createdAt: -1 });
 
     return NextResponse.json(issues);
